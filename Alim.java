37332ec404e8543614761b26a7fef73e7d4688f8
maria22-Alim <mfalim@paterostechnologicalcollege.edu.ph>
2025-03-03 20:50:23 +0800

    Create Alim.java

---
+++ Alim.java
@@ -0,0 +1,10 @@
+public class Alim {
+    public static void display() {
+        int age = 21; // Replace with user input if necessary
+        System.out.println("Hello! This is Cruz contributing to the project.");
+        System.out.println("My current age is: " + age);
+        System.out.println("Next year, I will be: " + (age + 1));
+
+      
+    }
+}
